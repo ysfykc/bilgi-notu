8bb90188b78cc0d4231fa456c019599815fada14
ysfykc <yusuf.aykac@gmail.com>
2026-05-24 15:20:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'bilginotu-v5';
-
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html']))
+    caches.open('bn-pro').then((cache) => cache.addAll(['./', 'index.html', 'manifest.json']))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match('./index.html'))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
