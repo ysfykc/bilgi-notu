ab6c5df0efd9f59062e7aadfad67e520c55a8f84
ysfykc <yusuf.aykac@gmail.com>
2026-05-24 19:49:02 +0300

    Update service worker caching logic

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
+const CACHE_NAME = 'bn-v7';
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('bn-pro').then((cache) => cache.addAll(['./', 'index.html', 'manifest.json']))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './manifest.json']))
   );
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
