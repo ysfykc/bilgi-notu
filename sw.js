3fd62d566c50cf613085f950353a3392922b884f
ysfykc <yusuf.aykac@gmail.com>
2026-05-24 11:47:05 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,36 @@
-const CACHE_NAME = 'bilginotu-v2';
+const CACHE_NAME = 'bilginotu-v4';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    }).then(() => self.skipWaiting())
+  );
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
 });
