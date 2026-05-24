911db66e99230ce1c828bdba02c8dce164383247
ysfykc <yusuf.aykac@gmail.com>
2026-05-24 21:20:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,23 @@
-const CACHE_NAME = 'bn-v9';
+const CACHE_NAME = 'bn-v10';
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './manifest.json']))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
   self.clients.claim();
 });
 
+// AĞ ÖNCELİKLİ (Önce internetten yenisini çekmeye çalışır, internet yoksa eskisini açar)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
